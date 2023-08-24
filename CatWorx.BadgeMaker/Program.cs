7bce0191e9d2737135b9343cc81b43b7fb3948cd
James A Cone <jacone626@gmail.com>
2023-08-24 16:21:04 -0400

    added Util class to make CSV file

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -30,10 +30,8 @@ namespace CatWorx.BadgeMaker
   }
   static void PrintEmployees(List<Employee> employees)
   {
-    for (int i = 0; i < employees.Count; i++) {
-      string template = "{0,-10}\t{1,-20}\t{2}";
-      Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
-    }
+    Util.PrintEmployees(employees);
+    Util.MakeCSV(employees);
   }
   static void Main(string[] args)
   {
