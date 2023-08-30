732f4526838119d292c61a3bd92ecedcb48a4e17
James A Cone <jacone626@gmail.com>
2023-08-30 13:59:16 -0400

    updated badge to show the employee and company name

--- CatWorx.BadgeMaker/Util.cs
+++ CatWorx.BadgeMaker/Util.cs
@@ -43,14 +43,47 @@ namespace CatWorx.BadgeMaker
     }
 
       async public static Task MakeBadges(List<Employee> employees) {
+
+        // Layout variables
+        int BADGE_WIDTH = 669;
+        int BADGE_HEIGHT = 1044;
+
+        int PHOTO_LEFT_X = 184;
+        int PHOTO_TOP_Y = 215;
+        int PHOTO_RIGHT_X = 486;
+        int PHOTO_BOTTOM_Y = 517;
+
+        int COMPANY_NAME_Y = 150;
+
+
         using(HttpClient client = new HttpClient())
     {
         for (int i = 0; i < employees.Count; i++)
         {
             SKImage photo = SKImage.FromEncodedData(await client.GetStreamAsync(employees[i].GetPhotoUrl()));
+            SKImage background = SKImage.FromEncodedData(File.OpenRead("badge.png"));
 
-            SKData data = photo.Encode();
+            SKBitmap badge = new SKBitmap(BADGE_WIDTH, BADGE_HEIGHT);
+            SKCanvas canvas = new SKCanvas(badge);
+
+            canvas.DrawImage(background, new SKRect(0, 0, BADGE_WIDTH, BADGE_HEIGHT));
+            canvas.DrawImage(photo, new SKRect(PHOTO_LEFT_X, PHOTO_TOP_Y, PHOTO_RIGHT_X, PHOTO_BOTTOM_Y));
+
+            SKPaint paint = new SKPaint();
+            paint.TextSize = 42.0f;
+            paint.IsAntialias = true;
+            paint.Color = SKColors.White;
+            paint.IsStroke = false;
+            paint.TextAlign = SKTextAlign.Center;
+            paint.Typeface = SKTypeface.FromFamilyName("Arial");
+
+            canvas.DrawText(employees[i].GetCompanyName(), BADGE_WIDTH / 2f, COMPANY_NAME_Y, paint);
+
+
+            SKImage finalImage = SKImage.FromBitmap(badge);
+            SKData data = finalImage.Encode();
             data.SaveTo(File.OpenWrite("data/employeeBadge.png"));
+        
         }
 
     }
